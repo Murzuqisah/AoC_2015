11c3d3d94ef1c8b1b2741b7edd0d9de411683672
jamos <amosjoel91@gmail.com>
2024-08-27 09:32:29 +0300

    refactor: refactor the main.go file feat: add test files for various cases

--- day_1/day1.go
+++ day_1/day1.go
@@ -1,56 +1,32 @@
-package main
+package day1
 
-import (
-	"os"
-	"fmt"
-)
-
-func main() {
-	argument := os.Args[0:]
-	if len(argument) < 2 {
-		return
-	}
-	file := argument[0]
-
-	input, err := os.ReadFile(file)
-	if err != nil {
-		fmt.Println("error reading file", err)
-	}
-
-	if len(input) < 1 {
-		fmt.Println("File is empty")
-	}
-
-	floor := FloorCount((input))
-	fmt.Println("Final floor: ", floor)
-
-}
-
-func FloorCount(s []byte) int{
+func FloorCount(s string) int {
 	/*
-	( -> 1 floor up
-	) -> 1 floor down
+		( -> 1 floor up
+		) -> 1 floor down
 	*/
-	floor := 0 
+	floor := 0
 	for _, char := range s {
 		if char == '(' {
-				floor++
-			} else if char == ')' {
-				floor--
-			}
+			floor++
+		} else if char == ')' {
+			floor--
+		}
 	}
 	return floor
 }
 
-// func FloorCount(floors []byte) int {
-// 	floor := map[byte]int {
-// 		'(' : 1,
-// 		')'	: -1,
+/*   ALTERNATIVE TWO    */
+
+// func FloorCount(floors string) int {
+// 	floor := map[byte]int{
+// 		'(': 1,
+// 		')': -1,
 // 	}
 
 // 	count := 0
 // 	for _, char := range floors {
-// 		if key, value := floor[char]; value {
+// 		if key, value := floor[byte(char)]; value {
 // 			count += key
 // 		}
 // 	}
---
+++ tests/day1_test.go
@@ -0,0 +1,72 @@
+package tests_test
+
+import (
+	"testing"
+
+	day1 "advent/day_1"
+)
+
+type Cases struct {
+	argument string
+	input    string
+	expected int
+}
+
+var Case = []Cases{
+	{
+		argument: "case1",
+		input:    "(())",
+		expected: 0,
+	},
+	{
+		argument: "case2",
+		input:    "()()",
+		expected: 0,
+	},
+	{
+		argument: "case3",
+		input:    "(((",
+		expected: 3,
+	},
+	{
+		argument: "case4",
+		input:    "(()(()(",
+		expected: 3,
+	},
+	{
+		argument: "case5",
+		input:    "))(((((",
+		expected: 3,
+	},
+	{
+		argument: "case6",
+		input:    "())",
+		expected: -1,
+	},
+	{
+		argument: "case7",
+		input:    "))(",
+		expected: -1,
+	},
+	{
+		argument: "case8",
+		input:    ")))",
+		expected: -3,
+	},
+	{
+		argument: "case9",
+		input:    ")())())",
+		expected: -3,
+	},
+}
+
+func TestFloorCount(t *testing.T) {
+	for _, tc := range Case {
+		t.Run(tc.argument, func(t *testing.T) {
+			actual := day1.FloorCount(tc.input)
+			if actual != tc.expected {
+				t.Errorf("expected %d but got %d", tc.expected, actual)
+			}
+		})
+	}
+}
