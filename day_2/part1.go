ce43040591314840e36658dfdfc1c9fcf1cf730b
jamos <amosjoel91@gmail.com>
2024-08-28 19:13:47 +0300

    refactor: refactor code to reflect requirements

--- day_2/part1.go
+++ day_2/part1.go
@@ -1,5 +1,10 @@
 package day2
 
+import (
+	"fmt"
+	"sort"
+)
+
 func Wrapping(dimensions []int) int {
 	if len(dimensions) != 3 {
 		return 0
@@ -9,8 +14,18 @@ func Wrapping(dimensions []int) int {
 	w := dimensions[1]
 	h := dimensions[2]
 
-	// length, width, height := value[0], value[1], value[2]
-	surfaceArea := 2*(l*w+w*h+h*l) + (l * w)
+	value1, value2 := Min(dimensions)
+	fmt.Println(value1, value2)
+
+	surfaceArea := 2*(l*w+l*h+h*w) + (value1 * value2)
 
 	return surfaceArea
+}
+
+func Min(values []int) (int, int) {
+	sort.Ints(values)
+
+	val1, val2 := values[0], values[1]
+
+	return val1, val2
 }
